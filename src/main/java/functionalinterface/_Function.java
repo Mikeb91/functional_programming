5057ce43a8ce33420c3b56d9c34bf1d95368b9bf
Mike Ballesteros <mike_b91@hotmail.com>
2021-06-01 07:19:32 -0500

    Changin Functions with method andThen()

--- src/main/java/functionalinterface/_Function.java
+++ src/main/java/functionalinterface/_Function.java
@@ -13,16 +13,24 @@ public class _Function {
         int multiply = multiplyBy10Function.apply(increment2);
         System.out.println(multiply);
 
-        
+        //We can do execute both functions at the same time with method andThen() as follows:
+
+        Function<Integer, Integer> addBy1AndThenMultiplyBy10 =
+                incrementByOneFunction.andThen(multiplyBy10Function);
+        System.out.println(addBy1AndThenMultiplyBy10.apply(1));
+
+        //As we can see here, the Function is build with another to functions one
+        // executed next to the other one with the use of method andThen()
+
     }
 
-    static Function<Integer,Integer> incrementByOneFunction =
+    static Function<Integer, Integer> incrementByOneFunction =
             number -> number + 1;
 
     static Function<Integer, Integer> multiplyBy10Function =
             number -> number * 10;
 
-    static int increment(int number){
+    static int increment(int number) {
         return number + 1;
     }
 }
